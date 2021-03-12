1a9520c34d141818b67212d5c5063aae23bc8ad5
DiFex-dev <78355557+DiFex-dev@users.noreply.github.com>
2021-03-13 02:49:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+	return 0;
+  }
+  let minimum=Infinity;
+  for(let i=0; i< array.length; i++){
+    if (array[i]<minimum)
+     minimum=array[i];
+  }
+  return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+	return 0;
+  }
+  let maximum=0;
+  for(let i=0; i< array.length; i++){
+    if (array[i]>maximum)
+     maximum=array[i];
+  }
+  return maximum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+	return 0;
+  }
+  if (array.length == 0)
+	return 0;
+  let sum=0;
+  for(let i=0; i<array.length; i++){
+  sum+=array[i];
+  }
+  return sum/array.length;
 }
